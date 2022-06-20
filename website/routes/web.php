16e201114de77ca45c7245274a3ac0e21154407b
mcleanka <mcleankasambala@gmail.com>
2022-06-20 08:39:40 +0200

    code refactoring

--- website/routes/web.php
+++ website/routes/web.php
@@ -1,20 +1,29 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+// Dashboard
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+// Users
+Route::get('users')->name('users')->uses('UsersController@index')->middleware('remember', 'auth');
+Route::get('users/create')->name('users.create')->uses('UsersController@create')->middleware('auth');
+Route::post('users')->name('users.store')->uses('UsersController@store')->middleware('auth');
+Route::get('users/{user}/edit')->name('users.edit')->uses('UsersController@edit')->middleware('auth');
+Route::put('users/{user}')->name('users.update')->uses('UsersController@update')->middleware('auth');
+Route::delete('users/{user}')->name('users.destroy')->uses('UsersController@destroy')->middleware('auth');
+Route::put('users/{user}/restore')->name('users.restore')->uses('UsersController@restore')->middleware('auth');
+
+// Images
+Route::get('/img/{path}', 'ImagesController@show')->where('path', '.*');
+
+// Reports
+// Route::get('reports')->name('reports')->uses('ReportsController')->middleware('auth');
+
+// 500 error
+Route::get('500', function () {
+    echo $fail;
 });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
