ab21ebe288f5e88fe106d7ee8e33d36aae5e2301
mcleanka <mcleankasambala@gmail.com>
2022-06-25 18:02:27 +0200

    updtaed dashboard router

--- website/routes/web.php
+++ website/routes/web.php
@@ -3,17 +3,20 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-// Dashboard
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+Route::middleware(["auth"])->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 // Users
-Route::get('users')->name('users')->uses('UsersController@index')->middleware('remember', 'auth');
-Route::get('users/create')->name('users.create')->uses('UsersController@create')->middleware('auth');
-Route::post('users')->name('users.store')->uses('UsersController@store')->middleware('auth');
-Route::get('users/{user}/edit')->name('users.edit')->uses('UsersController@edit')->middleware('auth');
-Route::put('users/{user}')->name('users.update')->uses('UsersController@update')->middleware('auth');
-Route::delete('users/{user}')->name('users.destroy')->uses('UsersController@destroy')->middleware('auth');
-Route::put('users/{user}/restore')->name('users.restore')->uses('UsersController@restore')->middleware('auth');
+    Route::get('users')->name('users')->uses('UsersController@index')->middleware('auth');
+    Route::get('users/create')->name('users.create')->uses('UsersController@create');
+    Route::post('users')->name('users.store')->uses('UsersController@store');
+    Route::get('users/{user}/edit')->name('users.edit')->uses('UsersController@edit');
+    Route::put('users/{user}')->name('users.update')->uses('UsersController@update');
+    Route::delete('users/{user}')->name('users.destroy')->uses('UsersController@destroy');
+    Route::put('users/{user}/restore')->name('users.restore')->uses('UsersController@restore');
+
+});
 
 // Images
 Route::get('/img/{path}', 'ImagesController@show')->where('path', '.*');
