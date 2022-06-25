040fb4a8a5452994fca3d3dd5b04d5994837d35d
mcleanka <mcleankasambala@gmail.com>
2022-06-25 18:02:04 +0200

    added seeder

---
+++ website/database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'id' => 1,
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'password' => Hash::make('1234'),
+                'status_id' => 1,
+            ],
+        ];
+
+        User::insert($users);
+
+    }
+}
