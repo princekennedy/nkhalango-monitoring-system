72677a64d751d45475bc08e2cffb64317ef6008c
mcleanka <mcleankasambala@gmail.com>
2022-06-26 08:54:50 +0200

    code refactoring

--- website/routes/web.php
+++ website/routes/web.php
@@ -1,20 +1,19 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get("/", function () {
+    return inertia("Home");
+});
 
 Route::middleware(["auth"])->group(function () {
     // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-// Users
-    Route::get('users')->name('users')->uses('UsersController@index')->middleware('auth');
-    Route::get('users/create')->name('users.create')->uses('UsersController@create');
-    Route::post('users')->name('users.store')->uses('UsersController@store');
-    Route::get('users/{user}/edit')->name('users.edit')->uses('UsersController@edit');
-    Route::put('users/{user}')->name('users.update')->uses('UsersController@update');
-    Route::delete('users/{user}')->name('users.destroy')->uses('UsersController@destroy');
-    Route::put('users/{user}/restore')->name('users.restore')->uses('UsersController@restore');
+    Route::put('users/{user}/restore')->name('user.restore')->uses('UsersController@restore');
+    Route::resource("users", UserController::class)->names("user");
 
 });
 
