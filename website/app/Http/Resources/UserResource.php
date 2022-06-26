de535bf89d53c80d2e8bccaad3d8c8ec038e6a09
mcleanka <mcleankasambala@gmail.com>
2022-06-26 08:54:30 +0200

    User api response

---
+++ website/app/Http/Resources/UserResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * The "data" wrapper that should be applied.
+     *
+     * @var string
+     */
+    // public static $wrap = 'user';
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'name' => $this->name,
+            'email' => $this->email,
+            // 'owner' => 234,
+            // 'photo' => $this->photo,
+            'deleted_at' => $this->deleted_at,
+            'status' => $this->whenLoaded('status'),
+        ];
+
+    }
+}
