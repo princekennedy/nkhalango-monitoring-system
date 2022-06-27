003abb2cc2998674920ab745864ee7bb39a2872b
mcleanka <mcleankasambala@gmail.com>
2022-06-27 08:44:15 +0200

    added system models

--- website/routes/web.php
+++ website/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\SoilController;
+use App\Http\Controllers\TreeSpeciesController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
@@ -9,23 +14,28 @@ Route::get("/", function () {
 });
 
 Route::middleware(["auth"])->group(function () {
-    // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+    /**
+     * users
+     */
     Route::put('users/{user}/restore')->name('user.restore')->uses('UsersController@restore');
     Route::resource("users", UserController::class)->names("user");
 
+    Route::resource("news", NewsController::class)->names("news");
+    Route::resource("soil", SoilController::class)->names("soil");
+    Route::resource("tree-species", TreeSpeciesController::class)->names("tree-species");
+    Route::resource("weather", WeatherController::class)->names("weather");
+
+    Route::resource("report", ReportController::class)->names("report");
 });
 
 // Images
 Route::get('/img/{path}', 'ImagesController@show')->where('path', '.*');
 
-// Reports
-// Route::get('reports')->name('reports')->uses('ReportsController')->middleware('auth');
-
 // 500 error
 Route::get('500', function () {
-    echo $fail;
+    echo "500";
 });
 
 require __DIR__ . '/auth.php';
