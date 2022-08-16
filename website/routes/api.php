24de54de543817f52ed73367ac3ce849db5c1874
mcleanka <mcleankasambala@gmail.com>
2022-08-16 10:13:31 +0200

    added system api routes

--- website/routes/api.php
+++ website/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\LabSessionController;
+use App\Http\Controllers\PopulationController;
+use App\Http\Controllers\WeatherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('lab-session/add', [LabSessionController::class, 'store']);
+Route::post('population/add', [PopulationController::class, 'store']);
+Route::post('weather/add', [WeatherController::class, 'store']);
