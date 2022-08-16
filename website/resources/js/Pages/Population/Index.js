ec2ba11330e4ebc3b6e62ff1945c971a6067348a
mcleanka <mcleankasambala@gmail.com>
2022-08-16 10:13:48 +0200

    updated components ui

--- website/resources/js/Pages/Population/Index.js
+++ website/resources/js/Pages/Population/Index.js
@@ -1,13 +1,27 @@
 import React from 'react'
-
 import App from '@/Layouts/App';
+import { Inertia } from '@inertiajs/inertia';
+import { usePage } from '@inertiajs/inertia-react';
 
-const Population = () => {
+const Index = () => {
+	const { population } = usePage().props;
+
+	const {
+		data: {
+			data
+		},
+		meta: { links }
+	} = population;
+
 	return <>
+		<div className="flex items-center justify-between mb-6">
+			<h1 className="text-3xl font-bold">Population Census</h1>
+		</div>
+
 
 	</>
 }
 
-Population.layout = page => <App title="Populations" children={page} />;
+Index.layout = page => <App title="Population" children={page} />;
 
-export default Population;
+export default Index;
